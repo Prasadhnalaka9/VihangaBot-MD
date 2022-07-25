18690fd67ea7a3a034a51cf9adf186e85f0ac1db
Prasadhnalaka9 <109862260+Prasadhnalaka9@users.noreply.github.com>
2022-07-25 14:44:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,9 +21,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94763427338']
-global.premium = ['94763427338']
-global.ownernomer = '94763427338'
+global.owner = ['94741959590']
+global.premium = ['94741959590']
+global.ownernomer = '94741959590'
 global.ownername = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
 global.botname = 'VIHANGA MD BOT'
 global.footer = 'VIHANGA MD'
